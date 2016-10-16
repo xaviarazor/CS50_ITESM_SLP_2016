647154f3668d77359ed569e9296d332a9125269b
xaviarazor <prietojavi551@gmail.com>
2016-10-15 23:26:25 -0500

    Create funciones_cs50.c

---
+++ IntroduccionALibrerias/funciones_cs50.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "funciones_cs50.h"
+/**
+ * hola()
+ * 
+ * Imprime hola arbitrariamente
+ */
+void hola(){
+    printf("hola!\n");
+}
+/**
+ * printInt(int a)
+ * 
+ * Imprime hola arbitrariamente
+ * 
+ * Argumentos:
+ * 'a': un argumento de tipo entero
+ */
+void printInt(int a){
+    printf("%i\n",a);
+}
